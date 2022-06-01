4822f3c7dadd533a5979e755d4fe80dd2dfe96d9
atothsmi <102501875+atothsmi@users.noreply.github.com>
2022-06-01 11:07:29 -0700

    Changes to how question functions and appearance

--- js/other-endangered.js
+++ js/other-endangered.js
@@ -24,27 +24,16 @@
      success: function(data) {
          // Get information from JSON object
          console.log(data.data);
-         //Set Current Animal to 0;
-         var currentAnimal = 0;
-         var windowHeight = window.innerHeight;
-         for (animal of data.data){
-           //Construct Div to display data from API
-           var display = '<div class="currently-endangered" id='+ currentAnimal + '>';
-           //Add link to website with more info to animal Name
-           display += '<a href="' + animal[1].url +'" target="_blank">' + animal[0] + '</a>'
-           //Add endangered status
-           display += '<p>Status: ' + animal[2] + '</p>'
-           $('#call-to-action').append(display);
-
-           //Set Random Distance from right side of screen
-           var randomWidth = Math.floor(Math.random() * (window.innerWidth - 200));
-           //Set Random Distance from the top of the screen
-           var randomHeight = Math.floor(Math.random() * (windowHeight - 200))
-           //Place div random distance in containing div
-           $('#' + currentAnimal).css('top', randomHeight);
-           $('#' + currentAnimal).css('right', randomWidth);
-           currentAnimal +=1
-         }
+         loadSlow(data)
+         .then(
+           function(value){
+             disappearClick();
+             console.log('done');
+           },
+           function(error){
+             console.log(error);
+           }
+         );
      },
      // What we do if the api call fails
      error: function (jqXHR, textStatus, errorThrown) {
@@ -55,10 +44,63 @@
  })
  }
 
+ //Asynchronous function timer from https://stackoverflow.com/questions/3583724/how-do-i-add-a-delay-in-a-javascript-loop
+ // Users: Jonas Wilms and Vsync
+ // Returns a Promise that resolves after "ms" Milliseconds
+ const timer = ms => new Promise(res => setTimeout(res, ms))
+
+ async function loadSlow (data) { //wrap the loop into an async function for this to work
+   //Set Current Animal to 0;
+   var currentAnimal = 0;
+   var windowHeight = window.innerHeight;
+   for (animal of data.data){
+     //Construct Div to display data from API
+     var display = '<div class="currently-endangered" id='+ currentAnimal + '>';
+     //Add link to website with more info to animal Name
+     display += '<a href="' + animal[1].url +'" target="_blank">' + animal[0] + '</a>'
+     //Add endangered status
+     display += '<p>Status: ' + animal[2] + '</p>'
+
+     //Append to the correct div
+     $('#call-to-action').append(display);
+
+     //Set Random Distance from right side of screen
+     var randomWidth = Math.floor(50 + (Math.random() * (window.innerWidth - 300)));
+     //Set Random Distance from the top of the screen
+     var randomHeight = Math.floor(50 + (Math.random() * (windowHeight - 300)))
+
+     //Create string id of current animal
+     var idCurrentAnimal = '#' + currentAnimal;
+     //Place div random distance in containing div
+     $(idCurrentAnimal).css('top', randomHeight);
+     $(idCurrentAnimal).css('right', randomWidth);
+
+     //Show div after it has been moved
+     $(idCurrentAnimal).show();
+
+    //Increment the current animal number
+     currentAnimal +=1
+
+     //Wait an amount of time
+     await timer(250); // then the created Promise can be awaited
+   }
+ }
+
+async function disappearClick(){
+  $('.currently-endangered').map(num => {
+
+    //console.log($('#' + num));
+    $('#' + num).click(function(){
+      console.log(num);
+      $('#' + num).hide();
+    });
+  });
+};
+
 //Find Question Element
 var questionEl = $('#question');
 
 //On click, summon info from api
-questionEl.click(function(){
-   getAndPutData(endpoint);
+questionEl.one('click', function(){
+  getAndPutData(endpoint);
 });
